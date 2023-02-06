b83db359fbd3a7a9104b9bbc3238a259e6d1123a
Danesh naik <99582894+daneshnaik@users.noreply.github.com>
2023-02-06 23:20:17 +0530

    Initial commit

---
+++ app/src/main/java/com/example/firebaselogin/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.firebaselogin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.android.material.textfield.TextInputLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
